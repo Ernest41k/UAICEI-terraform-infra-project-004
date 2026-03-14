82823b472ffc4ffbd82dcb86a92ab8fc397c8df1
Ernest <apcitestgithub@gmail.com>
2026-03-14 16:32:21 -0600

    add bug fix

--- rds/rds.tf
+++ rds/rds.tf
@@ -69,4 +69,6 @@ resource "aws_db_instance_role_association" "rds_secrets_manager_role" {
   db_instance_identifier = aws_db_instance.rds_mysql.id
   feature_name           = "secretsManager"
   role_arn               = var.rds_secrets_manager_role
+
+  depends_on = [aws_db_instance.rds_mysql]
 }
