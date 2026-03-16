85e7323042ff385eeda92cbff723eee258a74839
Ernest41k <136405174+Ernest41k@users.noreply.github.com>
2026-03-16 14:09:31 -0600

    Update project tag value in locals.tf

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   project_tags = {
     contact      = "devops@uaicei.com"
     application  = "Jupiter"
-    project      = "UAICEI"
+    project      = "uaicei"
     environment  = "${terraform.workspace}" # refers to your current workspace (dev, prod, etc)
     creationTime = timestamp()
   }
